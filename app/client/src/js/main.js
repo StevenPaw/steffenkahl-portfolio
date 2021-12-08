b3a8a7f2cb1477d2b88286c772c6ebf40d6408a8
StevenPaw <stevenpaw209@gmail.com>
2021-12-08 12:46:45 +0100

    [ADDED] Project

---
+++ app/client/src/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+
+    //Show hide
+    let listToggleElements = [...document.querySelectorAll('[data-behaviour="list-toggle"]')];
+
+    listToggleElements.forEach((element) => {
+        element.addEventListener("click", (e) => {
+            e.preventDefault();
+            const item = element.parentNode.parentNode;
+            item.classList.toggle("list_item--visible")
+
+            listToggleElements.filter(e => e.parentNode.parentNode != item).forEach((e) => {
+                e.parentNode.parentNode.classList.remove("list_item--visible")
+            });
+        })
+    });
+
+
+});
