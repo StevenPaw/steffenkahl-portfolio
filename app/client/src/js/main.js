f6485efa9ab6a8e78d02efd71997014cba1d6ca3
Steffen Kahl <stevenpaw209@gmail.com>
2022-04-30 15:49:50 +0200

    [ADDED] Main Menu

--- app/client/src/js/main.js
+++ app/client/src/js/main.js
@@ -17,4 +17,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
 
 
+    const menu_button = document.querySelector('[data-behaviour="toggle-menu"]');
+
+    menu_button.addEventListener('click', () => {
+        document.body.classList.toggle('body--show');
+    })
 });
